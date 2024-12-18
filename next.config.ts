92b8fb41bf9d77ae0d839f321c0a3c39d16dd0b3
RaysoLee <raysoli@163.com>
2024-12-18 21:24:56 +0800

    webpack config

--- next.config.ts
+++ next.config.ts
@@ -8,5 +8,13 @@ export default {
         pathname: '/s/files/**'
       }
     ]
-  }
+  },
+  webpack(config: any, { isServer }: { isServer: boolean }) {
+    // 如果是客户端构建，进行压缩
+    if (!isServer) {
+      config.optimization.splitChunks.maxSize = 2000000;  // 设置拆分的 chunk 文件大小
+    }
+    return config;
+  },
+  // output: 'export',
 };
